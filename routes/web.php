96382ff98404d39a94828e365b88c904cf59f823
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-06-05 11:00:37 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\AnimationController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,22 +18,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-})->name('home');
+Route::get('/',[HomeController::class,'returnPage'])->name('home');
 
-Route::get('/animation', function () {
-    return view('animation');
-})->name('animation');
+Route::get('/animation',[AnimationController::class,'returnPage'])->name('animation');
 
-Route::get('/tasks', function () {
-    return view('tasks');
-})->name('tasks');
+Route::get('/tasks',[TasksController::class,'returnPage'])->name('tasks');
 
-Route::get('/profile', function () {
-    return view('profile');
-})->name('profile');
+Route::get('/profile',[ProfileController::class,'returnPage'])->name('profile');
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
+Route::get('/contact',[ContactController::class,'returnPage'])->name('contact');
