ab600a4b028bdfdc65b1dba584229b6cabc7c785
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-06-05 09:48:03 +0800

    Add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('template.app')
+
+@section('title')
+    Home
+@endsection
+
+@section('heading-title')
+    Home
+@endsection
+
+@section('paragraph-content')
+    Welcome to home page :D
+@endsection
