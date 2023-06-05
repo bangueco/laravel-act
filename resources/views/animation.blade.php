e83a1d78a007a1c232d71689a259994811458c72
ivandnd <121280104+ivandnd@users.noreply.github.com>
2023-06-05 09:50:23 +0800

    Add animation page

---
+++ resources/views/animation.blade.php
@@ -0,0 +1,13 @@
+@extends('template.app')
+
+@section('title')
+    Animation
+@endsection
+
+@section('heading-title')
+    Animation
+@endsection
+
+@section('paragraph-content')
+    Welcome to animation page :D
+@endsection
